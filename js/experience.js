e616c67c922070485e76fea6d3997f10ab1365ab
EdgarPozas <edgar730a@gmail.com>
2021-01-11 10:37:21 -0700

    intento

---
+++ js/experience.js
@@ -0,0 +1,88 @@
+var app=new Vue({
+    el:"#experience-list",
+    data:{
+        "experiences":[
+            {
+                "place":"Centro de Investigación Científica y de Educación Superior de Ensenada Unidad de Transferencia Tecnológica de Tepic (CICESE-UT³)",
+                "year":"2020",
+                "position":"Líder de desarrollo",
+                "period":"ENERO 2020 - DICIEMBRE 2020",
+                "skills":[
+                    {"title":"Gestión de equipo","img":"team"},
+                    {"title":"Desarrollo de videojuegos","img":"games"},
+                    {"title":"Desarrollo web","img":"web"},
+                    {"title":"Documentación","img":"document"},
+                ]
+            },
+            {
+                "place":"CICESE-UT³",
+                "year":"2019",
+                "position":"Líder de desarrollo",
+                "period":"ENERO 2019 - DICIEMBRE 2019",
+                "skills":[
+                    {"title":"Gestión de equipo","img":"team"},
+                    {"title":"Desarrollo de videojuegos","img":"games"},
+                    {"title":"Desarrollo de scripts","img":"scripts"},
+                ]
+            },
+            {
+                "place":"CICESE-UT³",
+                "year":"2018",
+                "position":"Líder de desarrollo",
+                "period":"ENERO 2018 - DICIEMBRE 2018",
+                "skills":[
+                    {"title":"Gestión de equipo","img":"team"},
+                    {"title":"Desarrollo de videojuegos","img":"games"},
+                    {"title":"Desarrollo web","img":"web"},
+                    {"title":"Documentación","img":"document"},
+                    {"title":"Desarrollo de scripts","img":"scripts"},
+                ]
+            },
+            {
+                "place":"Instituto Nacional de Antropología e Historia (INAH) de Nayarit",
+                "year":"2018",
+                "position":"Programador",
+                "period":"AGOSTO  2018 - DICIEMBRE 2018",
+                "skills":[
+                    {"title":"Desarrollo web","img":"web"},
+                    {"title":"Documentación","img":"document"},
+                    {"title":"Uso de Google Maps","img":"maps"},
+                ]
+            },
+            {
+                "place":"Consultorio Aguilar",
+                "year":"2018",
+                "position":"Programador",
+                "period":"AGOSTO  2018 - DICIEMBRE 2018",
+                "skills":[
+                    {"title":"Desarrollo de un sistema","img":"web"},
+                    {"title":"Documentación","img":"document"},
+                    {"title":"Uso de estadísticas","img":"stats"},
+                ]
+            },
+            {
+                "place":"CICESE-UT³",
+                "year":"2017 / 2015",
+                "position":"Desarrollador principal",
+                "period":"ENERO 2015 - DICIEMBRE 2017",
+                "skills":[
+                    {"title":"Desarrollo de videojuegos","img":"games"},
+                    {"title":"Documentación","img":"document"},
+                    {"title":"Desarrollo de scripts","img":"scripts"},
+                ]
+            },
+            {
+                "place":"Secretaría de Comunicaciones y Transportes de Tepic Nayarit",
+                "year":"2014",
+                "position":"Servicio social",
+                "period":"AGOSTO 2014 - DICIEMBRE 2014",
+                "skills":[
+                    {"title":"Desarrollo de un sistema","img":"web"},
+                    {"title":"Documentación","img":"document"},
+                ]
+            },
+        ]
+    }
+
+
+});
---
+++ js/habilities.js
@@ -0,0 +1,39 @@
+var app=new Vue({
+    el:"#habilities-list",
+    data:{
+        "habilities":[
+            {
+                "title":"Programación web",
+                "stars":[1,1,1,0]
+            },
+            {
+                "title":"Programación móvil",
+                "stars":[1,1,0,0]
+            },
+            {
+                "title":"Programación de escritorio",
+                "stars":[1,1,1,0]
+            },
+            {
+                "title":"Programación de scripting",
+                "stars":[1,1,1,0]
+            },
+            {
+                "title":"Gestión y manejo de bases de datos",
+                "stars":[1,1,0,0]
+            },
+            {
+                "title":"Redes de computadoras",
+                "stars":[1,1,,0]
+            },
+            {
+                "title":"Gestión de proyectos",
+                "stars":[1,1,0,0]
+            },
+            {
+                "title":"Inglés",
+                "stars":[1,1,1,0]
+            }
+        ]
+    }
+});
